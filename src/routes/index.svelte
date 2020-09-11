f2a9d7211bae4ad869810764c00f9cd8c5d9cf3f
Louis Quinnell <_@ziorobo.co>
2020-09-11 22:07:20 +1000

    Remove all template styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,3 @@
-<style>
-  h1 {
-    text-align: center;
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
-
 <svelte:head>
   <title>ziorobo.co</title>
 </svelte:head>

c235fe683e9ba3a0e157dbe85cb9dea97be41dfb
Louis Quinnell <_@ziorobo.co>
2020-09-13 01:54:26 +1000

    Remove footer, clean up date formatting

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,14 +2,7 @@
   import Tailwind from "../styles/Tailwind.svelte"
   import Global from "../styles/Global.svelte"
   import Header from "../components/Header.svelte"
-  import Footer from "../components/Footer.svelte"
 </script>
-
-<style>
-  main {
-    margin-top: 2em;
-  }
-</style>
 
 <Tailwind />
 <Global />
@@ -18,4 +11,3 @@
 <main>
   <slot />
 </main>
-<Footer />
--- src/routes/_post.svelte
+++ src/routes/_post.svelte
@@ -1,7 +1,20 @@
 <script>
   export let title
   export let date
+
+  const { format } = new Intl.DateTimeFormat()
+
+  $: date = {
+    iso: date,
+    pretty: format(new Date(date)),
+  }
 </script>
+
+<style>
+  article > footer {
+    @apply mt-4 mb-16 text-xs;
+  }
+</style>
 
 <svelte:head>
   <title>{title}</title>
@@ -10,9 +23,7 @@
 <article>
   <h1><a href="/posts/{title}">{title}</a></h1>
   <slot />
-  {#if date}
-    <br />
-    <time
-      datetime={date}>{new Intl.DateTimeFormat().format(new Date(date))}</time>
-  {/if}
+  <footer>
+    {#if date}<time datetime={date.iso}>{date.pretty}</time>{/if}
+  </footer>
 </article>

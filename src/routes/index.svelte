2c209dd3c2f7277c1cb59f1cd5ddd7b9d83f51e4
Louis Quinnell <_@ziorobo.co>
2020-12-28 19:38:56 +1100

    Revert to original content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,9 @@
 <script>
-  import Hello from "./posts/hello.svx"
+  import Ding from "./posts/ding.svx"
 </script>
 
 <svelte:head>
   <title>ziorobo.co</title>
 </svelte:head>
 
-<Hello />
-
-<footer>
-  <hr />
-  <h1>other posts</h1>
-  <ul>
-    <li><a href="/posts/ding">ding</a></li>
-  </ul>
-</footer>
+<Ding />
